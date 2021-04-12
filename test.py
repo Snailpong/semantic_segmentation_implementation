e852d8a74d94eacae27c7e628d1bb5fe41d3677f
Snailpong <tjdtnsu@gmail.com>
2021-04-12 21:13:38 +0900

    Add: Evaluate metric

--- test.py
+++ test.py
@@ -22,11 +22,13 @@ NUM_CLASSES = 21
 @click.command()
 @click.option('--model_name', default='pspnet')
 @click.option('--image_path', default='./data/VOCdevkit/VOC2012/JPEGImagesVal')
-def test(model_name, image_path):
+@click.option('--evaluate', type=click.BOOL, default=True)
+def test(model_name, image_path, evaluate):
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device: {}'.format(device))
 
+    evaluate_list = []
     os.makedirs('./result', exist_ok=True)
 
     model = getModel(model_name, NUM_CLASSES)
@@ -45,7 +47,8 @@ def test(model_name, image_path):
         files_list = [image_path]
 
     for idx, file_path in enumerate(files_list):
-        print('{}/{} {}'.format(idx, len(files_list), file_path), end='\r')
+        file_name = '.'.join(os.path.basename(file_path).split('.')[:-1])
+        print('\r{}/{} {}'.format(idx, len(files_list), file_name), end=' ')
 
         image = Image.open(file_path)
         image_size = image.size
@@ -60,10 +63,28 @@ def test(model_name, image_path):
         output = torch.argmax(torch.squeeze(output), dim=0)
         output = Image.fromarray(np.array(output, dtype=np.uint8), 'L')
         output = np.array(output.resize(image_size, Image.NEAREST))
-        
+
+        if evaluate:
+            ground_truth_path = './data/VOCdevkit/VOC2012/SegmentationClassAug/{}.png'.format(file_name)
+            ground_truth = np.array(Image.open(ground_truth_path), dtype=np.int)
+            ground_truth[ground_truth == 255] = 0
+            output_evaluate = output.copy()
+            output_evaluate[np.where(ground_truth == 0)] = 0
+
+            union = ground_truth | output_evaluate
+            intersection = ground_truth & output_evaluate
+
+            iou = intersection.sum() / union.sum()
+            print(iou, end='')
+            evaluate_list.append(iou)
+
         outputColorMap = segmentationColorize(output, colorMap)
         outputColorMap = Image.fromarray(outputColorMap, 'RGB')
-        outputColorMap.save('./result/{}.jpg'.format('.'.join(os.path.basename(file_path).split('.')[:-1])))
+        outputColorMap.save('./result/{}.jpg'.format(file_name))
+
+    if evaluate:
+        print()
+        print(np.mean(evaluate_list), np.std(evaluate_list))
 
 
 if __name__ == '__main__':
